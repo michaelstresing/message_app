cd8278546a584dd7a02115c016379106053dd4f0
michaelstresing <mstresing@gmail.com>
2019-11-04 10:19:23 +0800

    working s3

--- application/fileuploads/connects3.py
+++ application/fileuploads/connects3.py
@@ -8,9 +8,9 @@ import os
 
 import logging
 import boto3
+from botocore.client import Config
 from botocore.exceptions import ClientError
 import requests
-
 
 FileAPI = Blueprint("file_api", __name__)
 
@@ -21,46 +21,51 @@ S3_LOCATION               = f'http://{S3_BUCKET}.s3.amazonaws.com/'
 
 s3_client = boto3.client('s3',
                         aws_access_key_id=S3_KEY,
-                        aws_secret_access_key=S3_SECRET
+                        aws_secret_access_key=S3_SECRET,
+                        config=Config(signature_version='s3v4'),
+                        region_name='us-east-2'
                         )
 
-
 @FileAPI.route('/upload/', methods=["PUT"])
-def upload_test(object_name='/Users/michaelstresing/Desktop/s3test.txt'):
+def upload_test(file_name='/Users/michaelstresing/Desktop/s3test.txt'):
+
+    object_name = "s3test.txt"
 
     # Generate a presigned S3 POST URL
-    # object_name = "/Users/michaelstresing/Desktop/s3test.txt"
-
     response = create_presigned_post(object_name)
     if response is None:
         exit(1)
 
-    # Demonstrate how another Python program can use the presigned URL to upload a file
-    
-    http_response = requests.put(response['url'],
+    http_response = requests.post(response['url'],
                                   data=response['fields'],
-                                  files={'file': open(object_name, 'rb')}
-                                  )
+                                  files={'file': (object_name, open(file_name, 'rb'))}
+                                )
 
     print(response['url'], response['fields'])
     
     # If successful, returns HTTP status code 204
     logging.info(f'File upload HTTP status code: {http_response.status_code}')
 
-    return f"{http_response}"
+    return f"{http_response.text}"
 
 def create_presigned_post(object_name,
                           fields=None, conditions=None, expiration=3600):
 
     try:
         response = s3_client.generate_presigned_post('quackio',
-                                                     object_name,
-                                                     Fields=fields,
-                                                     Conditions=conditions,
-                                                     ExpiresIn=expiration)
+                                                        object_name,
+                                                        Fields = fields,
+                                                        Conditions = conditions,
+                                                        ExpiresIn = 3600
+                                                    )
     except ClientError as e:
         logging.error(e)
         return None
 
     # The response contains the presigned URL and required fields
     return response
+
+
+# @FileAPI.route('/retrieve/')
+# def get_test(file_name='/Users/michaelstresing/Desktop/s3test.txt')
+
